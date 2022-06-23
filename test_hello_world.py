149ecdb750024537dee816d261ea12e5cffe1227
Blake Burns <71161414+bburns8@users.noreply.github.com>
2022-06-23 16:51:49 -0700

    Update test_hello_world.py

--- test_hello_world.py
+++ test_hello_world.py
@@ -15,7 +15,7 @@ class TestHelloWorld(unittest.TestCase):
     def test_message(self):
         response = self.app.get('/')
         message = hello_world.wrap_html('Hello DockerCon 2018!')
-        self.assertEqual(response.data, message)
+        self.assertEqual(message, response.data)
 
 if __name__ == '__main__':
     unittest.main()
